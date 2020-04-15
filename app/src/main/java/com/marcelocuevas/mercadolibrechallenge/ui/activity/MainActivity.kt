8c45a2afc6dd9bcea947147f8a7f21301c496bac
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-04-15 14:11:27 -0300

    Fragments created

--- app/src/main/java/com/marcelocuevas/mercadolibrechallenge/ui/activity/MainActivity.kt
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/ui/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.marcelocuevas.mercadolibrechallenge.ui
+package com.marcelocuevas.mercadolibrechallenge.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/ui/fragment/ProductsListFragment.kt
@@ -0,0 +1,8 @@
+package com.marcelocuevas.mercadolibrechallenge.ui.fragment
+
+import androidx.fragment.app.Fragment
+
+class ProductsListFragment: Fragment() {
+
+
+}
