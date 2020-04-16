c6d47e3922068ef500e48f5c9fce986e0b48ab9d
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-04-15 21:21:07 -0300

    Koin started

---
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/di.kt
@@ -0,0 +1,15 @@
+package com.marcelocuevas.mercadolibrechallenge
+
+import org.koin.dsl.module
+
+val dataModule = module {
+
+}
+
+val useCaseModule = module {
+
+}
+
+val appModule = module {
+
+}
---
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/ui/viewmodel/ProductListViewModel.kt
@@ -0,0 +1,9 @@
+package com.marcelocuevas.mercadolibrechallenge.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ProductListViewModel: ViewModel() {
+
+
+
+}
