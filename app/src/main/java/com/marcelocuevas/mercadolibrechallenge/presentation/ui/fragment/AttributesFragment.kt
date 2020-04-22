489a6a3a75f2f7bcf0801cc470f635880f7f9e6f
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-04-21 23:17:36 -0300

    Reviews and Attributes Fragments created

---
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/presentation/ui/fragment/AttributesFragment.kt
@@ -0,0 +1,15 @@
+package com.marcelocuevas.mercadolibrechallenge.presentation.ui.fragment
+
+import com.marcelocuevas.mercadolibrechallenge.R
+import com.marcelocuevas.mercadolibrechallenge.databinding.FragmentAttributesBinding
+
+class AttributesFragment: DataBindingFragment<FragmentAttributesBinding>() {
+
+    override fun layoutRes(): Int = R.layout.fragment_attributes
+
+    override fun init() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/presentation/ui/fragment/ReviewsFragment.kt
@@ -0,0 +1,15 @@
+package com.marcelocuevas.mercadolibrechallenge.presentation.ui.fragment
+
+import com.marcelocuevas.mercadolibrechallenge.R
+import com.marcelocuevas.mercadolibrechallenge.databinding.FragmentReviewsBinding
+
+class ReviewsFragment: DataBindingFragment<FragmentReviewsBinding>() {
+
+    override fun layoutRes(): Int = R.layout.fragment_reviews
+
+    override fun init() {
+
+    }
+
+
+}
