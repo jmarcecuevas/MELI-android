50186b1d32b8b4f14626acf8154d17ea63b11d86
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-04-23 11:46:58 -0300

    Timber initialized

--- app/src/main/java/com/marcelocuevas/mercadolibrechallenge/App.kt
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/App.kt
@@ -4,16 +4,30 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        startKoin()
+        startTimber()
+    }
+
+    private fun startKoin() {
         startKoin {
             androidLogger()
             androidContext(this@App)
             modules(listOf(dataModule, useCaseModule, appModule))
+        }
+    }
+
+    private fun startTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
         }
     }
 }
