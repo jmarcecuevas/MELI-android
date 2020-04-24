cc33088170b8c5d2a436e3a479140bb540f61b3a
Juan Marcelo Cuevas <marce.cuevas16@gmail.com>
2020-04-24 12:20:41 -0300

    ConnectivityInterceptor fix

--- app/src/main/java/com/marcelocuevas/mercadolibrechallenge/framework/network/ConnectivityInterceptorImpl.kt
+++ app/src/main/java/com/marcelocuevas/mercadolibrechallenge/framework/network/ConnectivityInterceptorImpl.kt
@@ -2,27 +2,61 @@ package com.marcelocuevas.mercadolibrechallenge.framework.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import okhttp3.Interceptor
 import okhttp3.Response
 import timber.log.Timber
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
 
 class ConnectivityInterceptorImpl(context: Context): ConnectivityInterceptor {
 
     private val appContext = context.applicationContext
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        if (!isOnline()) {
+        return if (!isConnectionOn()) {
             Timber.w("No internet connection")
             throw NoConnectivityException()
+        } else if(!isInternetAvailable()) {
+            Timber.w("No internet connection")
+            throw NoConnectivityException()
+        } else {
+            chain.proceed(chain.request())
         }
-        return chain.proceed(chain.request())
     }
 
-    private fun isOnline(): Boolean {
-        val connectivityManager: ConnectivityManager =
-            appContext.getSystemService(Context.CONNECTIVITY_SERVICE)
-            as ConnectivityManager
-        val networkInfo = connectivityManager.activeNetworkInfo
-        return networkInfo != null && networkInfo.isConnected
+    private fun isConnectionOn(): Boolean {
+        val connectivityManager = appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            val network = connectivityManager.activeNetwork
+            val connection = connectivityManager.getNetworkCapabilities(network)
+            return connection != null && (
+                    connection.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                            connection.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+        } else {
+            val activeNetwork = connectivityManager.activeNetworkInfo
+            if (activeNetwork != null) {
+                return (activeNetwork.type == ConnectivityManager.TYPE_WIFI ||
+                        activeNetwork.type == ConnectivityManager.TYPE_MOBILE)
+            }
+            return false
+        }
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        return try {
+            val timeoutMs = 1500
+            val sock = Socket()
+            val sockaddr = InetSocketAddress("8.8.8.8", 53)
+
+            sock.connect(sockaddr, timeoutMs)
+            sock.close()
+
+            true
+        } catch (e: IOException) {
+            false
+        }
     }
 }
